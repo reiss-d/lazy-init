160438cf9b44f673c5b51a07b4b99c39cf2719c4
reiss-d <reiss-d@outlook.com>
2023-09-18 22:50:03 +0100

    initial package setup

---
+++ packages/plugin-esbuild/jest.config.js
@@ -0,0 +1,26 @@
+// @ts-check
+
+/** @type {import('jest').Config} */
+const config = {
+   displayName: 'plugin-esbuild',
+   testEnvironment: 'node',
+   testMatch: ['**/tests/**/?(*.)+(spec|test).[jt]s?(x)'],
+   transform: {
+      '^.+\\.tsx?$': ['@swc/jest', {
+         jsc: {
+            target: 'es2020',
+            parser: { 'syntax': 'typescript' },
+            experimental: {
+               plugins: [
+                  ['@lazy-init/plugin-swc-v83', {}],
+               ],
+            },
+         },
+      }],
+   },
+   // since the actual package.json is in dist (publishConfig.directory)
+   // we need to ignore the root package.json so jest doesn't complain
+   modulePathIgnorePatterns: ['<rootDir>/package.json'],
+}
+
+module.exports = config
