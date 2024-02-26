b0366b1af8c0f23a5f97589b4e140ee56f7b3745
reiss-d <reiss-d@outlook.com>
2024-02-26 21:14:53 +0000

    refactor(eslint-plugin-lazy-init): align major version with `@typescript-eslint/*`

--- packages/eslint-plugin-lazy-init/src/rules/index.ts
+++ packages/eslint-plugin-lazy-init/src/rules/index.ts
@@ -1,12 +1,5 @@
-// import requireAwait from './require-await/base'
-// import requireAwaitTyped from './require-await/typed'
 import requireAwait from './require-await'
 
 export default {
    'require-await': requireAwait as any,
 }
-
-// export default {
-//    'require-await': requireAwait as any,
-//    'require-await-typed': requireAwaitTyped as any,
-// }
--- packages/eslint-plugin-lazy-init/tests/RuleTester.ts
+++ packages/eslint-plugin-lazy-init/tests/RuleTester.ts
@@ -5,11 +5,5 @@ function getFixturesRootDir(): string {
    return path.join(__dirname, 'fixtures')
 }
 
-const { batchedSingleLineTests, RuleTester, noFormat } = ESLintUtils
-export {
-   InvalidTestCase,
-   RunTests,
-   ValidTestCase,
-} from '@typescript-eslint/utils/dist/eslint-utils/rule-tester/RuleTester'
-
-export { batchedSingleLineTests, getFixturesRootDir, noFormat, RuleTester }
+const { RuleTester } = ESLintUtils
+export { getFixturesRootDir, RuleTester }
